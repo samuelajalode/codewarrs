60608236d8fab732e756c2fb20613f9f1fb12e32
samuelajalode <113217825+samuelajalode@users.noreply.github.com>
2023-01-27 13:52:55 +0100

    first comm to CODEWARSMultiply.js

---
+++ 8kyu/Multiply.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+// This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+  a * b
+}
+
+// SOLUTION:
+
+function multiply(a, b){
+    return a * b
+    }
+console.log(multiply(2, 3));
+
+
