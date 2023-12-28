06be98e53c94ff93b6e7ee0316f47e11f66208c3
samuelajalode <113217825+samuelajalode@users.noreply.github.com>
2023-12-28 07:37:44 +0100

    an addtoTakeaTenMinutesWalktjngjhkbshe

--- 6kyu/TakeaTenMinutesWalk.js
+++ 6kyu/TakeaTenMinutesWalk.js
@@ -30,7 +30,7 @@ return false;*/
 
 function isValidWalk(walk) {
     function count(val) {
-      return walk.filter(function(a){return a==val;}).length;
+      return walk.filter((a) => a==val).length;
     }
     return walk.length==10 && count('n')==count('s') && count('w')==count('e');
   }
