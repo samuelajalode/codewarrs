99508a9b4ad18075e28f2bac5cb5c0479e1efa17
samuelajalode <113217825+samuelajalode@users.noreply.github.com>
2023-12-28 08:51:29 +0100

    an addtotictactoejRRhkbshgte

--- 5kyu/Tic-Tac-ToeChecker.js
+++ 5kyu/Tic-Tac-ToeChecker.js
@@ -1,7 +1,10 @@
 // DESCRIPTION:
-// If we were to set up a Tic-Tac-Toe game, we would want to know whether the board's current state is solved, wouldn't we? Our goal is to create a function that will check that for us!
+// If we were to set up a Tic-Tac-Toe game, 
+// we would want to know whether the board's current state is solved, 
+// wouldn't we? Our goal is to create a function that will check that for us!
 
-// Assume that the board comes in the form of a 3x3 array, where the value is 0 if a spot is empty, 1 if it is an "X", or 2 if it is an "O", like so:
+// Assume that the board comes in the form of a 3x3 array, where the value is 0 if a spot is empty, 
+// 1 if it is an "X", or 2 if it is an "O", like so:
 
 // [[0, 0, 1],
 //  [0, 1, 2],
